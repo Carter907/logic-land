fefa76b9807183097c3416f6318efff53a99d4b9
Carter <speers.carter@gmail.com>
2025-02-01 16:57:54 -0500

    continual experimentation2

--- src/lib/page/loadpage.server.ts
+++ src/lib/page/loadpage.server.ts
@@ -3,7 +3,7 @@ import path from 'path';
 
 export async function getSource(url: { pathname: string }) {
 
-  const filePath = path.join("markdown-pages", `${url.pathname}.md`);
+  const filePath = path.join("/markdown-pages", `${url.pathname}.md`);
   const contents = fs.readFileSync(filePath, 'utf-8');
 
   return {
