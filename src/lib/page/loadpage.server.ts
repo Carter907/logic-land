14cd09e12f81170ee7468d5c069596160332db00
Carter <speers.carter@gmail.com>
2025-02-01 17:02:43 -0500

    continual experimentation3

--- src/lib/page/loadpage.server.ts
+++ src/lib/page/loadpage.server.ts
@@ -3,6 +3,16 @@ import path from 'path';
 
 export async function getSource(url: { pathname: string }) {
 
+  fs.readdir(process.cwd(), (err, files) => {
+    if (err) {
+      return console.error('Unable to scan directory: ' + err);
+    }
+
+    console.log('Files in directory:');
+    files.forEach((file) => {
+      console.log(file);
+    });
+  });
   const filePath = path.join("/markdown-pages", `${url.pathname}.md`);
   const contents = fs.readFileSync(filePath, 'utf-8');
 
